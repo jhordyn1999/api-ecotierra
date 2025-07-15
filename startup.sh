5e1cf653287fc3391c4f15e976c4312a64f4ff60
jhordynBCT <jmontenegro@bctecnologia.com>
2025-07-15 16:06:46 -0500

    cambiando el startup

--- startup.sh
+++ startup.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# Copia tu configuración personalizada de nginx
-cp /home/site/wwwroot/nginx.conf /etc/nginx/sites-available/default
-
-# Inicia PHP-FPM en segundo plano
-service php8.2-fpm start
-
-# Ejecuta NGINX en primer plano para mantener vivo el contenedor
-nginx -g "daemon off;"
+php -S 0.0.0.0:80 -t /home/site/wwwroot/public
